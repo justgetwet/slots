f483e446caafe8219739e093d547a196d9f51325
AkiraWin <frog7bar@gmail.com>
2021-11-03 13:55:44 +0900

    pmf

--- jug.py
+++ jug.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numba import njit
+from concurrent.futures import ProcessPoolExecutor
 from scipy.stats import binom
 
 #@njit("i8[:](i8,i8,i8)", cache=True)
@@ -25,35 +26,74 @@ def imJuggler(s, trial, game):
 
   return ret
 
-def reg_binomial(s):
-  np.random.seed(0)
+def im_binomial(s, trial, game):
+  # np.random.seed(0)
   s = s - 1
-  trial = 10000
-  game = 2000
-  rb = 439.8, 399.6, 331.0, 315.1, 255.0, 255.0
-  p = np.reciprocal(rb)
-  ret = np.random.binomial(game, p[s], trial)
-  return ret
+  p = np.empty((2, 6))
+  p[0] = 273.1, 269.7, 269.7, 259.0, 259.0, 255.0 # big
+  p[1] = 439.8, 399.6, 331.0, 315.1, 255.0, 255.0 # reg
+  q = np.reciprocal(p)[1]
 
-def spam(n):
-  game = 8000
-  rb = 439.8, 399.6, 331.0, 315.1, 255.0, 255.0
-  p = np.reciprocal(rb)
-  pmf = []
-  for s in [1,2,3,4,5,6]:
-    s = s - 1
-    pmf.append(binom.pmf(n, game, p[s]))
+  return np.random.binomial(game, q[s], trial)
+
+def im_pmf(n, game):
   
-  return pmf 
+  p = np.empty((2, 6))
+  p[0] = 273.1, 269.7, 269.7, 259.0, 259.0, 255.0 # big
+  p[1] = 439.8, 399.6, 331.0, 315.1, 255.0, 255.0 # reg
+  q = np.reciprocal(p)[1]
+  ret = [binom.pmf(n, game, q[s]) for s in range(6)]
+  
+  return ret 
+
+def sim_pmf(n, game, settings):
+
+  p = np.empty((2, 6))
+  p[0] = 273.1, 269.7, 269.7, 259.0, 259.0, 255.0 # big
+  p[1] = 439.8, 399.6, 331.0, 315.1, 255.0, 255.0 # reg
+  q = np.reciprocal(p)[1]
+  pmfs = [binom.pmf(n, game, q[s-1]) for s in settings]
+  for s, p in zip(settings, pmfs):
+    print(s, round(p/sum(pmfs), 2))
+
+
+def spam():
+  pmf = im_pmf(15, 3000)
+  for i, p in enumerate(pmf):
+    print(i+1, round(p/sum(pmf), 2))
 
 if __name__ == '__main__':
 
-  pmf = spam(30)
-  # a = imJuggler(s=1, trial=10000, game=2000)
-  # b = imJuggler(s=6, trial=100000, game=2000)
-  #c = reg_binomial(1)
-  # plt.hist([a, c], bins=16, label=["a", "c"])
-  # plt.hist(c)
-  plt.bar([1,2,3,4,5,6], pmf)
+  trial = 100000
+  game = 3000
+  # with ProcessPoolExecutor(max_workers=2) as executor:
+  #   futures = []
+  #   settings = [1,2,3,4,5,6]
+  #   for s in  settings:
+  #     future = executor.submit(im_binomial, s, trial, game)
+  #     print(future)
+  #     futures.append(future)
+
+  #   b = [f.result() for f in futures]
+  #   plt.hist(b, label=settings)
+  
+  # plt.title(f"imJugller reg in {game} games")
   # plt.legend()
-  plt.show()
+  # # plt.savefig("reg3000")
+  # plt.show()
+
+  pmf = im_pmf(15, game)
+  print(pmf)
+  p = (pmf[5])/sum(pmf)
+  print("事前確率", round(1/6, 2))
+  print("事後確率", round(p, 2))
+  plt.title(f"imJugller 15 regs in {game} games")
+  plt.bar([1,2,3,4,5,6], pmf)
+  # plt.savefig("pmf3000")
+  # plt.show()
+  
+  sim_pmf(15, game, [2,2,2,2,3,3,3,4])
+  
+  # PH = 1/6
+  # print(PH)
+  # print((PH * 0.36) / ((PH * 0.36) + (PH * 0.64)))
